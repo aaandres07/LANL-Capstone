ede5bcef61399290029f4233c74b67cefa8f36d4
shiionnnnn <seanlin2987@gmail.com>
2025-04-01 14:55:35 -0500

    cal

--- MEEN/motors/master_arduino/master_arduino.ino
+++ MEEN/motors/master_arduino/master_arduino.ino
@@ -99,17 +99,17 @@ void controlDrive(int l, int r) {
   leftMotor.writeMicroseconds(l); rightMotor.writeMicroseconds(r);
 }
 
-void controlNet(int speed) {
+void controlNet(int speed) { //net motors calibrated (4/01/25)
   if (speed > 0) {
     // extend net
     digitalWrite(IN1, HIGH); digitalWrite(IN2, LOW);
     digitalWrite(IN3, HIGH); digitalWrite(IN4, LOW);
-    analogWrite(ENA, 255); analogWrite(ENB, 255); // Calibration Left (255); Right (255)
+    analogWrite(ENA, 255); analogWrite(ENB, 230); // Calibration Left (255); Right (240)
   } else if (speed < 0) {
     // retract net
     digitalWrite(IN1, LOW); digitalWrite(IN2, HIGH);
     digitalWrite(IN3, LOW); digitalWrite(IN4, HIGH);
-    analogWrite(ENA, 255); analogWrite(ENB, 255); // Calibration Left (255); Right (255)
+    analogWrite(ENA, 255); analogWrite(ENB, 241); // Calibration Left (255); Right (240)
   } else {
     // stop
     digitalWrite(IN1, LOW); digitalWrite(IN2, LOW);
@@ -118,17 +118,17 @@ void controlNet(int speed) {
   }
 }
 
-void controlActuator(int cmd) { //linear actuators calibrated (3/30/25)
+void controlActuator(int cmd) { //linear actuators calibrated (4/01/25)
   if (cmd == 1) {
     // up
     digitalWrite(ACT1_IN1, HIGH); digitalWrite(ACT1_IN2, LOW);
     digitalWrite(ACT2_IN1, HIGH); digitalWrite(ACT2_IN2, LOW);
-    analogWrite(ACT_ENA, 255); analogWrite(ACT_ENB, 250); // Calibration Left (255); Right (250)
+    analogWrite(ACT_ENA, 255); analogWrite(ACT_ENB, 251); // Calibration Left (255); Right (251)
   } else if (cmd == 2) {
     // down
     digitalWrite(ACT1_IN1, LOW); digitalWrite(ACT1_IN2, HIGH);
     digitalWrite(ACT2_IN1, LOW); digitalWrite(ACT2_IN2, HIGH);
-    analogWrite(ACT_ENA, 250); analogWrite(ACT_ENB, 255); // Calibration Left (250); Right (255)
+    analogWrite(ACT_ENA, 251); analogWrite(ACT_ENB, 255); // Calibration Left (251); Right (255)
   } else {
     // stop
     digitalWrite(ACT1_IN1, LOW); digitalWrite(ACT1_IN2, LOW);
